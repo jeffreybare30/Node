e75d35bbe34d7b53157e14270bdee9eb4a4b4134
Jeff Bare (work laptop) <jeffrey.bare@gmail.com>
2019-07-08 15:33:40 -0500

    Added in pieces to read and save to a JSON file

--- app.js
+++ app.js
@@ -11,19 +11,23 @@ var command = process.argv[2];
 
 console.log(process.argv);
 console.log('Command: ', command);
-console.log('Process', process.argv);
+//console.log('Process', process.argv);
 console.log('Yargs', argv);
+var command = argv._[0];
 
 
 if (command === 'add'){
     //console.log('Adding new note');
     notes.addNote(argv.title, argv.body);
 } else if (command === 'list'){
-    console.log('Listing all notes');
+    //console.log('Listing all notes');
+    notes.getAll();
 } else if (command === 'read'){
-    console.log('Fetching Notes');
+    //console.log('Fetching Notes');
+    notes.getNote(argv.title);
 } else if (command === 'remove'){
-    console.log('Deleting Notes');
+    //console.log('Deleting Notes');
+    notes.removeNote(argv.title);
 } else {
     console.log('Command not recognized');
 }
--- notes.js
+++ notes.js
@@ -1,21 +1,49 @@
 console.log('Starting notes.js');
 //console.log(module);
 
-module.exports.age = 33;
+//module.exports.age = 33;
 
 /*module.exports.addNote = ()=>{
     console.log('addNote');
     return 'New note';
 };
 */
+const fs = require('fs');
 
 var addNote = (title, body) => {
     //console.log('Title: ', title);
     //console.log('Note: ', body);
-    console.log('Adding note', title, body);
+    //console.log('Adding note', title, body);
+
+    var notesString = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(notesString);
+    
+
+    var notes = [];
+    var note = {
+        title,
+        body
+    };
+
+    notes.push(note);
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
 };
+
+var getAll = () => {
+    console.log('Getting all the notes');
+};
+
+var getNote = (title) => {
+    console.log('Getting note: ', title);
+};
+
+var removeNote = (title) => {
+    console.log('Removing note: ', title);
+}
 
 module.exports = {
-    addNote: addNote
+    addNote,
+    getAll,
+    getNote,
+    removeNote
 };
-
